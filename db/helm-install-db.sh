07569113f3db95b2bc855b14062ca13608689729
Giuseppe Capizzi <gcapizzi@pivotal.io>
2020-04-15 10:56:15 +0000

    Update helm repos before installing MySQL

--- db/helm-install-db.sh
+++ db/helm-install-db.sh
@@ -2,6 +2,7 @@
 
 kubectl apply -f ./serviceaccount.yml
 
+helm repo update
 helm upgrade pheed-mysql stable/mysql --install  \
   --namespace pheed-db \
   --set persistence.storageClass=hostpath,mysqlDatabase=pheed,service.type=NodePort
