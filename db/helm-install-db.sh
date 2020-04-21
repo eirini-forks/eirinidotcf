963abf1d74d414ef278634838467f1c1c17aead2
Julian Skupnjak <skupnjak@de.ibm.com>
2020-04-21 13:54:28 +0000

    Use existing secrets when deploying db
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- db/helm-install-db.sh
+++ db/helm-install-db.sh
@@ -2,8 +2,13 @@
 
 kubectl apply -f ./serviceaccount.yml
 
+useExistingSecret=false
+if helm status pheed-mysql; then
+    useExistingSecret=true
+fi
+
 helm init --client-only
 helm repo update
 helm upgrade pheed-mysql stable/mysql --install  \
   --namespace pheed-db \
-  --set persistence.storageClass=hostpath,mysqlDatabase=pheed,service.type=NodePort
+  --set persistence.storageClass=hostpath,mysqlDatabase=pheed,service.type=NodePort,existingSecret="$useExistingSecret"
