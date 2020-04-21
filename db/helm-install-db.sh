2ff8b5d69f3e6ae33b385463ab8cd41acc9d9c31
Julian Skupnjak <skupnjak@de.ibm.com>
2020-04-21 14:13:01 +0000

    Use only existing mysql root password insted of entire secret
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- db/helm-install-db.sh
+++ db/helm-install-db.sh
@@ -2,13 +2,13 @@
 
 kubectl apply -f ./serviceaccount.yml
 
-useExistingSecret="nil"
+MYSQL_ROOT_PASSWORD=""
 if helm status pheed-mysql; then
-    useExistingSecret="pheed-mysql"
+    MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace pheed-db pheed-mysql -o jsonpath="{.data.mysql-root-password}" | base64 --decode; echo)
 fi
 
 helm init --client-only
 helm repo update
 helm upgrade pheed-mysql stable/mysql --install  \
   --namespace pheed-db \
-  --set persistence.storageClass=hostpath,mysqlDatabase=pheed,service.type=NodePort,existingSecret="$useExistingSecret"
+  --set persistence.storageClass=hostpath,mysqlDatabase=pheed,service.type=NodePort,mysqlRootPassword="$MYSQL_ROOT_PASSWORD"
